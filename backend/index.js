48e9ee12add2935a9e5856b3d3ca812a6799da02
Ouezghari Assia <ouezghariassia@MacBook-Air-de-Ouezghari-2.local>
2026-04-17 18:32:42 +0200

    initialisation du backend

---
+++ backend/index.js
@@ -0,0 +1,37 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+dotenv.config();
+import { UserRouter } from "./routes/user.js";
+import authRoutes from './routes/auth.routes.js';
+import adminRoutes from './routes/admin.routes.js';
+import treeRoutes from './routes/tree.routes.js';
+
+const app = express();
+app.use(express.json());
+app.use(
+  cors({
+    origin: function (origin, callback) {
+
+      callback(null, true);
+    },
+    credentials: true,
+  })
+);
+app.use(cookieParser());
+app.use("/user", UserRouter);
+app.use('/auth', authRoutes);
+app.use('/admin', adminRoutes);
+app.use('/tree', treeRoutes);
+
+app.get("/", ( _ , res) => {
+  res.send("Hello World");
+});
+mongoose.connect(
+ process.env.MONGO_URI,
+);
+app.listen(process.env.PORT, () => {
+  console.log(`Server is Running on port ${process.env.PORT}`);
+});
